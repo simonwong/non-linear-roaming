03a5a72ba574eb1e4bfaf75ad9cf4f7af48ffe8f
Simon <wsj_simon@163.com>
2025-04-20 18:48:17 +0800

    feat: init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { withContentCollections } from "@content-collections/next";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withContentCollections(
+  defineCloudflareConfig(
+    {
+      incrementalCache: r2IncrementalCache,
+      ...nextConfig
+    }
+  )
+);
